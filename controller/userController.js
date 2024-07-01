8c615043bf43b63f02cc2db7ca25cae1fb2ceb0a
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2024-07-01 12:56:30 +0530

    add update user route

--- controller/userController.js
+++ controller/userController.js
@@ -6,80 +6,91 @@ const sendEmail = require('../middleware/nodeMailer');
 const { OTPgenrator, storeOTP, verifyOTP } = require("../controller/otp");
 
 exports.register = async (req, res) => {
-    try {
-        const { username, password, email, number, address, location, country } = req.body;
-        const securePassword = await bcrypt.hash(password, 10);
+  try {
+    const { username, password, email, number, address, location, country } = req.body;
+    const securePassword = await bcrypt.hash(password, 10);
 
-        const result = await client.query('INSERT INTO users (username, password, email, number, address, location, country) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-            [username, securePassword, email, number, address, location, country])
+    const result = await client.query('INSERT INTO users (username, password, email, number, address, location, country) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [username, securePassword, email, number, address, location, country])
 
-        const payload = {
-            id: result.id,
-            email: result.email
-        }
-        const token = generateToken(payload);
-        console.log("Token is : ", token);
-
-        res.status(200).json({ response: result.rows[0], token: token });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    const payload = {
+      id: result.id,
+      email: result.email
     }
+    const token = generateToken(payload);
+    console.log("Token is : ", token);
+
+    res.status(200).json({ response: result.rows[0], token: token });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 exports.login = async (req, res) => {
-    try {
-        const { email, number, password } = req.body;
-        if (!email && !number || !password) {
-            res.status(400).json({ error: "plese chack input field" });
-        }
-        const result = await client.query('SELECT * FROM users WHERE email = $1 OR number = $2 ', [email, number]);
-        if (result.rows.length === 0) {
-            res.status(300).json({ error: "user not found" });
-        };
-
-        const user = result.rows[0];
-        const correctPassword = await bcrypt.compare(password, user.password);
-        if (!correctPassword) {
-            res.status(400).json({ error: "invalid password" });
-        }
-        const payload = {
-            id: user.id,
-            email: user.email
-        }
-        const token = generateToken(payload);
-        console.log("token is", token);
-
-        res.status(201).json({ message: "login successful", token: token });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { email, number, password } = req.body;
+    if (!email && !number || !password) {
+      res.status(400).json({ error: "plese chack input field" });
     }
+    const result = await client.query('SELECT * FROM users WHERE email = $1 OR number = $2 ', [email, number]);
+    if (result.rows.length === 0) {
+      res.status(300).json({ error: "user not found" });
+    };
+
+    const user = result.rows[0];
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      res.status(400).json({ error: "invalid password" });
+    }
+    const payload = {
+      id: user.id,
+      email: user.email
+    }
+    const token = generateToken(payload);
+    console.log("token is", token);
+
+    res.status(201).json({ message: "login successful", token: token });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+      const {password, username, email, number, address, location, country } = req.body;
+      
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 exports.generateOTP = async (req, res) => {
-    try {
-        const { Email } = req.body;
-        const result = await client.query('SELECT * FROM users WHERE Email = $1', [Email]);
-        const user = result.rows[0];
+  try {
+    const { Email } = req.body;
+    const result = await client.query('SELECT * FROM users WHERE Email = $1', [Email]);
+    const user = result.rows[0];
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
-        // Generate OTP
-        const otp = OTPgenrator();
-        const email = user.email;
-        console.log(otp);
-        storeOTP(email, otp);
-        // Send OTP via email
-        const emailMessage = {
-            from: process.env.EMAIL_USER,
-            to: user.email,
-            subject: 'Your OTP Online Book Store Account',
-            html: `
+    // Generate OTP
+    const otp = OTPgenrator();
+    const email = user.email;
+    console.log(otp);
+    storeOTP(email, otp);
+    // Send OTP via email
+    const emailMessage = {
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: 'Your OTP Online Book Store Account',
+      html: `
         <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
             <h2>OTP for Online Book Store Account</h2>
             <p>Your One-Time Password is <strong>${otp}</strong></p>
@@ -87,75 +98,74 @@ exports.generateOTP = async (req, res) => {
             <br>
             <p>Online Book Store</p>
         </div>`
-        };
+    };
 
-        sendEmail.sendMail(emailMessage, (error, info) => {
-            if (error) {
-                console.error('Email send error:', error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
+    sendEmail.sendMail(emailMessage, (error, info) => {
+      if (error) {
+        console.error('Email send error:', error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
 
-        res.status(200).json({ message: 'OTP sent successfully' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    res.status(200).json({ message: 'OTP sent successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
 exports.forgotpassword = async (req, res) => {
-    try {
-      const { email, otp, newPassword } = req.body;
-  
-      if (!email) {
-        return res.status(400).json({ error: "Please provide an email." });
-      }
-  
-      if (!verifyOTP(email, otp)) {
-        return res.status(401).json({ error: 'Invalid OTP' });
-      }
-  
-      const securePassword = await bcrypt.hash(newPassword, 10);
-      const result = await client.query('UPDATE users SET password = $1 WHERE email = $2 RETURNING *', [securePassword, email]);
-      const user = result.rows[0];
-  
-      if (!user) {
-        return res.status(404).json({ error: "User not found." });
-      }
-  
-      return res.status(200).json({ message: "Password reset successfully." });
-  
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { email, otp, newPassword } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: "Please provide an email." });
     }
-  };
-  
+
+    if (!verifyOTP(email, otp)) {
+      return res.status(401).json({ error: 'Invalid OTP' });
+    }
+
+    const securePassword = await bcrypt.hash(newPassword, 10);
+    const result = await client.query('UPDATE users SET password = $1 WHERE email = $2 RETURNING *', [securePassword, email]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    return res.status(200).json({ user: user, message: "Password reset successfully." });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 exports.deleteAccount = async (req, res) => {
-    try {
-      const { email, otp } = req.body;
-  
-      if (!email) {
-        return res.status(400).json({ error: "Please provide an email." });
-      }
-  
-      if (!verifyOTP(email, otp)) {
-        return res.status(401).json({ error: 'Invalid OTP' });
-      }
-  
-      const result = await client.query('DELETE FROM users WHERE email = $1 RETURNING *', [email]);
-      const user = result.rows[0];
-  
-      if (!user) {
-        return res.status(404).json({ error: "User not found." });
-      }
-  
-      return res.status(200).json({ message: "User deleted successfully." });
-  
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { email, otp } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: "Please provide an email." });
     }
-  };
+
+    if (!verifyOTP(email, otp)) {
+      return res.status(401).json({ error: 'Invalid OTP' });
+    }
+
+    const result = await client.query('DELETE FROM users WHERE email = $1 RETURNING *', [email]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    return res.status(200).json({ message: "User deleted successfully." });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, generateOTP, forgotpassword, deleteAccount } = require("../controller/userController");
+const { register, login, updateUser, generateOTP, forgotpassword, deleteAccount } = require("../controller/userController");
 
 router.post("/signup", register);
 router.post("/login", login);
+router.put("/updateUser", updateUser);
 router.post("/generateotp", generateOTP);
 router.delete("/delete", deleteAccount);
 router.put("/forgotpassword", forgotpassword);
